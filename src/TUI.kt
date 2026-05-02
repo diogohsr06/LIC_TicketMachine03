5791d9fe5686e51ebc3d3e6fb71bf726f1bfb3c3
diogohsr06 <diogohsr2006@gmail.com>
2026-05-02 17:01:50 +0100

    TUI done. All interfaces implemented. Moving to stations

--- src/TUI.kt
+++ src/TUI.kt
@@ -42,11 +42,47 @@ object TUI {
     }
     fun yesOrNo(text: String, time: Long): Boolean {
         write(text, 0, 0, true, true)
-        write("*-Yes  other-No", 1, 0, true, false)
+        write("*-Yes   other-No", 1, 0, true, false)
         return KBD.waitKey(time) == '*'
     }
-    fun printTicket(station: String, rt: Boolean, price: Double) {
-        TODO()
+    fun vendingAborted() {
+        write("Vending Aborted.", 0, 0, true, true)
+    }
+    fun vendingAborted2(price: Double) {
+        write("Vending Aborted.", 0, 0, true, true)
+        write("Returned ${"%.2f".format(price)}${0.toChar()}", 1, 0, true, false)
+    }
+    fun collectTicket(station: String) {
+        write(station, 0, 0, true, true)
+        write("Collect Ticket", 1, 0, true, false)
+    }
+    fun collectFinished() {
+        write("Thank You!", 0, 0, true, true)
+        write("Have a nice trip", 1, 0, true, false)
+    }
+    fun toPrint(station: String, rt: Boolean) {
+        write(station, 0, 0, true, true)
+        if (rt) write("${1.toChar()} *- to Print", 1, 0, true, false)
+        if (!rt) write("${1.toChar()}${2.toChar()} *- to Print", 1, 0, true, false)
+    }
+    fun printTicket(station: String, keyCode: Int, price: Double) {
+        write(station, 0, 0, true, true)
+        write("${"%02d".format(keyCode)}${1.toChar()}${2.toChar()}       ${"%.2f".format(price)}${0.toChar()}"
+            , 1, 0, true, false)
+    }
+    fun processing(station: String) {
+        write(station, 0, 0, true, true)
+        write("Processing ...", 1, 0, true, false)
+    }
+    fun printCoins(value: Double, amount: Int, code: Int) {
+        write("${"%.2f".format(value)}${0.toChar()}", 0, 0, true, true)
+        write("${"%02d".format(code)}${1.toChar()}${2.toChar()}           $amount",
+            1, 0, false, false)
+    }
+    fun stationCount(station: String, count: Int) {
+        write(station, 0, 0, true, true)
+        write("${"%02d".format(count)}${1.toChar()}${2.toChar()}           $count"
+            , 1, 0, false, false)
     }
 }
 
@@ -64,19 +100,35 @@ fun main() {
 
     TUI.write("Test2: Yes/No", 0, 0, true, true)
     Time.sleep(1000)
-    val text = "die."
+    val text = "Fumar faz bem?"
     LCD.clear()
     val yN = TUI.yesOrNo(text,5000)
     if (yN) {
         TUI.write("You chose Yes", 0, 0, true, true)
     } else {
-        TUI.write("nigga.", 0, 0, true, true)
+        TUI.write("Faz sim.", 0, 0, true, true)
     }
     Time.sleep(5000)
 
-    TUI.write("Test3: Printing Tickets", 0, 0, true, true)
+    TUI.write("Test3: Printing Tickets Interfaces", 0, 0, true, true)
     Time.sleep(1000)
-    TUI.printTicket("Lisboa", true, 2.25)
-    Time.sleep(3000)
-    TUI.printTicket("Maldivas", false, 4.55)
+    TUI.printTicket("Lisboa", 1, 2.25)
+    Time.sleep(2000)
+    TUI.printTicket("Maldivas", 13, 4.55)
+    Time.sleep(2000)
+    TUI.vendingAborted()
+    Time.sleep(2000)
+    TUI.vendingAborted2(3.46)
+    Time.sleep(2000)
+    TUI.collectTicket("Roma")
+    Time.sleep(2000)
+    TUI.collectFinished()
+    Time.sleep(2000)
+    TUI.toPrint("Atenas", true)
+    Time.sleep(2000)
+    TUI.processing("Troia")
+    Time.sleep(2000)
+    TUI.printCoins(2.55, 4, 8)
+    Time.sleep(2000)
+    TUI.stationCount("Wakanda", 2)
 }
