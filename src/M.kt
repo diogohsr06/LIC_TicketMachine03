e12eb9c0b9baa57be51d14e04d9c98ea7f3c0ace
diogohsr06 <diogohsr2006@gmail.com>
2026-05-04 20:00:08 +0100

    M simply checks for hardware info if M mode is on

--- src/M.kt
+++ src/M.kt
@@ -1,7 +1,18 @@
 import isel.leic.utils.Time
 /**Maintenance**/
 object M {
+    fun enabled() = HAL.isBit(INPUTPORTS.M_OUT.mask)
     fun init() {
         HAL.init()
     }
+}
+
+/**Teste**/
+fun main() {
+    M.init()
+    println("M state: ${M.enabled()}")
+    Time.sleep(1000)
+    println("Enable M in hardware")
+    Time.sleep(5000)
+    println("M state: ${M.enabled()}")
 }
